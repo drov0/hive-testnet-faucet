5b2f8878f3ed8c52a23dc6b9248c19e2eb13935e
Martin Lees <drov0@users.noreply.github.com>
2019-11-03 13:12:49 +0100

    v1

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
+require("dotenv").config();
+const steem = require('steem');
+const bodyParser = require('body-parser')
+const express = require('express');
+const exphbs  = require('express-handlebars');
 
-var app = express();
+let chain_id = "0feb08c380aeb483b61a34cccb7271a3a99c47052bea529c4a891622f2c50d75";
 
+steem.api.setOptions({url: 'https://testnet.steemitdev.com/', useAppbaseApi :  true, address_prefix : 'TST', 'chain_id' : chain_id});
+steem.config.set('address_prefix', 'TST');
+steem.config.set('chain_id', chain_id);
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.render('home');
+function transfer(wif, from, to, amount)
+{
+    return new Promise(resolve => {
+
+        steem.broadcast.transfer(wif, from, to, amount, "", function (err, result) {
+            if (!err)
+            {
+                return resolve({status : "ok"})
+            } else {
+                return resolve({err : err})
+            }
+        });
+    });
+}
+
+app.get('/', async function (req, res) {
+    let ctx = {};
+    let account = (await steem.api.callAsync('condenser_api.get_accounts', [[process.env.STEEM_USER]]))[0]
+
+    ctx.tests = account.balance;
+    ctx.chain_id = chain_id;
+
+    res.render('home', ctx);
+});
+
+app.post('/', async function (req, res) {
+    let ctx = {};
+
+    let username = req.body.username;
+    let result = await transfer(process.env.STEEM_ACTIVE, process.env.STEEM_USER, username, "5.000 TESTS");
+    let account = (await steem.api.callAsync('condenser_api.get_accounts', [[process.env.STEEM_USER]]))[0];
+
+    ctx.result = result.status === "ok";
+    ctx.err = result.err !== undefined ? result.err.message : false;
+    ctx.tests = account.balance;
+    ctx.chain_id = chain_id;
+    ctx.username = username;
+
+    res.render('home', ctx);
 });
 
 app.listen(3000);
