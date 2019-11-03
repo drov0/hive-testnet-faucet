3b2647d2539317da14604bfa4c9ea425bfa698bc
Martin Lees <drov0@users.noreply.github.com>
2019-11-03 19:05:05 +0100

    limitation of 50 steem per 6 hours

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const steem = require('steem');
 const bodyParser = require('body-parser')
 const express = require('express');
 const exphbs  = require('express-handlebars');
+let JsonDB  = require('node-json-db').JsonDB;
+let Config = require('node-json-db/dist/lib/JsonDBConfig').Config
 
-let chain_id = "0feb08c380aeb483b61a34cccb7271a3a99c47052bea529c4a891622f2c50d75";
+var jsondb = new JsonDB(new Config("db", true, false, '/'));
+
+let chain_id = process.env.CHAIN_ID;
 
 steem.api.setOptions({url: 'https://testnet.steemitdev.com/', useAppbaseApi :  true, address_prefix : 'TST', 'chain_id' : chain_id});
 steem.config.set('address_prefix', 'TST');
@@ -48,18 +52,56 @@ app.post('/', async function (req, res) {
     let username = req.body.username;
     let account = (await steem.api.callAsync('condenser_api.get_accounts', [[process.env.STEEM_USER]]))[0];
 
+    ctx.tests = account.balance;
+    ctx.chain_id = chain_id;
+    ctx.username = username;
+
+    let user_data;
+
+    try {
+        user_data = jsondb.getData("/" + username);
+    } catch (e) {
+
+    }
+
+
+    if (user_data !== undefined)
+    {
+        // if it has been less than 6 hours since the last claim
+        if (((new Date().getTime()/1000 - user_data.time) < 28800)) {
+            if (user_data.claimed > 50)
+            {
+                ctx.err = "You already reach the claim limit of 50 TESTS tokens in 6 hours, please try again after a few hours";
+                return  res.render('home', ctx);
+            }
+        } else
+        {
+            // reset claimed count
+            user_data = undefined;
+        }
+    }
+
     let result;
     if (parseFloat(account.balance) > 100) {
-         result = await transfer(process.env.STEEM_ACTIVE, process.env.STEEM_USER, username, "5.000 TESTS");
+        result = await transfer(process.env.STEEM_ACTIVE, process.env.STEEM_USER, username, "5.000 TESTS");
     } else {
         result = {err : {message : "Faucet's balance can't go lower than 100 TESTS"} }
     }
 
-    ctx.result = result.status === "ok";
+    if (result.status === "ok")
+    {
+        if (user_data === undefined)
+            jsondb.push("/"+username, {time : new Date().getTime()/1000, claimed : 5});
+        else {
+            user_data.claimed += 5;
+            jsondb.push("/"+username, user_data);
+        }
+        ctx.result = true;
+        ctx.tests = (parseFloat(ctx.tests) - 5) + " TESTS";
+    }
+
+
     ctx.err = result.err !== undefined ? result.err.message : false;
-    ctx.tests = account.balance;
-    ctx.chain_id = chain_id;
-    ctx.username = username;
 
     res.render('home', ctx);
 });
