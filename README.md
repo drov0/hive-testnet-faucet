753f57a1179c4cd8b397c3dca154b2af8a8d54dd
Martin Lees <drov0@users.noreply.github.com>
2021-05-25 19:42:32 -0400

    updated to use hive and a lot of improvements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Steem Testnet Faucet
+# Hive Testnet Faucet
 
 This website allows you to claim testnet tokens. Keep in mind that those tokens are worthless! They are only here for testing purposes
-Input your username and you will recieve 5 TESTS (equivalent of STEEM on the testnet).
+Input your username and you will recieve 5 TESTS (equivalent of HIVE on the testnet).
 
 ## Installing 
 
@@ -10,8 +10,8 @@ Input your username and you will recieve 5 TESTS (equivalent of STEEM on the tes
 then create a .env file containing these fields :
 ```
 CHAIN_ID=testnet_chain_id
-STEEM_USER=you_faucet_account
-STEEM_ACTIVE=your_faucet_account_active_key
+HIVE_USER=you_faucet_account
+HIVE_ACTIVE=your_faucet_account_active_key
 ```
 ## Running it
 
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
-const steem = require('steem');
-const bodyParser = require('body-parser')
+const hive = require('@hiveio/hive-js');
+const bodyParser = require('body-parser');
 const express = require('express');
 const exphbs  = require('express-handlebars');
 let JsonDB  = require('node-json-db').JsonDB;
-let Config = require('node-json-db/dist/lib/JsonDBConfig').Config
+let Config = require('node-json-db/dist/lib/JsonDBConfig').Config;
 
 var jsondb = new JsonDB(new Config("db", true, false, '/'));
 
 let chain_id = process.env.CHAIN_ID;
 
-steem.api.setOptions({url: 'https://testnet.steemitdev.com/', useAppbaseApi :  true, address_prefix : 'TST', 'chain_id' : chain_id});
-steem.config.set('address_prefix', 'TST');
-steem.config.set('chain_id', chain_id);
+hive.api.setOptions({url: 'https://testnet.openhive.network/', address_prefix : 'TST', 'chain_id' : chain_id});
+hive.config.set('address_prefix', 'TST');
+hive.config.set('chain_id', chain_id);
 
 const app = express();
 
@@ -21,27 +21,43 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-function transfer(wif, from, to, amount)
+function transfer_tokens(wif, from, to, amount)
 {
     return new Promise(resolve => {
+        const ops = [];
 
-        steem.broadcast.transfer(wif, from, to, amount, "", function (err, result) {
-            if (!err)
-            {
+        ops.push(['transfer', {
+            from,
+            to,
+            amount: `${amount} TBD`,
+            memo: "Enjoy your faucet tokens :)"
+        }]);
+        ops.push(['transfer', {
+            from,
+            to,
+            amount: `${amount} TESTS`,
+            memo: "Enjoy your faucet tokens :)"
+        }]);
+
+        hive.broadcast.send({operations: ops, extensions: []}, {posting: wif}, async function (err) {
+            if (!err) {
                 return resolve({status : "ok"})
             } else {
                 return resolve({err : err})
             }
         });
+
     });
 }
 
 app.get('/', async function (req, res) {
     let ctx = {};
-    let account = (await steem.api.callAsync('condenser_api.get_accounts', [[process.env.STEEM_USER]]))[0]
+    let account = (await hive.api.callAsync('condenser_api.get_accounts', [[process.env.HIVE_USER]]))[0];
 
     ctx.tests = account.balance;
+    ctx.tbd = account.hbd_balance;
     ctx.chain_id = chain_id;
+    ctx.given_tokens = process.env.AMOUNT_GIVEN;
 
     res.render('home', ctx);
 });
@@ -50,11 +66,13 @@ app.post('/', async function (req, res) {
     let ctx = {};
 
     let username = req.body.username;
-    let account = (await steem.api.callAsync('condenser_api.get_accounts', [[process.env.STEEM_USER]]))[0];
+    let account = (await hive.api.callAsync('condenser_api.get_accounts', [[process.env.HIVE_USER]]))[0];
 
     ctx.tests = account.balance;
+    ctx.tbd = account.hbd_balance;
     ctx.chain_id = chain_id;
     ctx.username = username;
+    ctx.given_tokens = process.env.AMOUNT_GIVEN;
 
     let user_data;
 
@@ -64,14 +82,13 @@ app.post('/', async function (req, res) {
 
     }
 
-
     if (user_data !== undefined)
     {
         // if it has been less than 6 hours since the last claim
         if (((new Date().getTime()/1000 - user_data.time) < 28800)) {
             if (user_data.claimed > 50)
             {
-                ctx.err = "You already reach the claim limit of 50 TESTS tokens in 6 hours, please try again after a few hours";
+                ctx.err = "You already reach the claim limit of 50 tokens in 6 hours, please try again after a few hours";
                 return  res.render('home', ctx);
             }
         } else
@@ -82,22 +99,23 @@ app.post('/', async function (req, res) {
     }
 
     let result;
-    if (parseFloat(account.balance) > 100) {
-        result = await transfer(process.env.STEEM_ACTIVE, process.env.STEEM_USER, username, "5.000 TESTS");
+    if (parseFloat(account.balance) > 100 && parseFloat(account.hbd_balance) > 100) {
+        result = await transfer_tokens(process.env.HIVE_ACTIVE, process.env.HIVE_USER, username, process.env.AMOUNT_GIVEN);
     } else {
-        result = {err : {message : "Faucet's balance can't go lower than 100 TESTS"} }
+        result = {err : {message : "Faucet's balance can't go lower than 100 TESTS or TBD"} }
     }
 
     if (result.status === "ok")
     {
         if (user_data === undefined)
-            jsondb.push("/"+username, {time : new Date().getTime()/1000, claimed : 5});
+            jsondb.push("/"+username, {time : new Date().getTime()/1000, claimed : parseFloat(process.env.AMOUNT_GIVEN)});
         else {
-            user_data.claimed += 5;
+            user_data.claimed += parseFloat(process.env.AMOUNT_GIVEN);
             jsondb.push("/"+username, user_data);
         }
-        ctx.result = true;
-        ctx.tests = (parseFloat(ctx.tests) - 5) + " TESTS";
+        ctx.result = process.env.AMOUNT_GIVEN;
+        ctx.tests = (parseFloat(ctx.tests) - parseFloat(process.env.AMOUNT_GIVEN)) + " TESTS";
+        ctx.tbd = (parseFloat(ctx.tbd) - parseFloat(process.env.AMOUNT_GIVEN)) + " TBD";
     }
 
 
